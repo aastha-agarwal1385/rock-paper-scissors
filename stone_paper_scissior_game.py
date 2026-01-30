d2b252e886d710ae2b4b0b467692193eaf8d66ba
Aastha-agarwal1385 <aasthaworkspace0209@gmail.com>
2026-01-30 22:58:01 +0530

    Modified result logic and improved code

--- stone_paper_scissior_game.py
+++ stone_paper_scissior_game.py
@@ -1,22 +1,26 @@
 import random
+
+# determines outcome of single round
 def game(entry,number):
     #for rock and scissior
     if entry=="r" and number==3:
-        return "you won this round"
-    elif entry=="s" and number==1:
-        return "you lost this round"   
+        return "won"
+    elif entry=="s" and number==1: 
+        return "lose" 
+
     # for scissior and paper
     elif entry=="s" and number==2:
-        return "you won this round"
+        return "won"
     elif entry=="p" and number==3:
-        return "you lost this round"  
+        return "lose" 
+
     # for paper and rock
     elif entry=="p" and number==1:
-        return "you won this round"
+        return "won"
     elif entry=="r" and number==2:
-        return "you lost this round"  
+        return "lose"  
     else:  
-        return "it's a tie this round!!"    
+        return "tie"    
 
 #INTRODUCTION AND RULES TO PLAY
 print("Hi there,we are going to play stone-paper-scissor ")
@@ -25,34 +29,51 @@ print("Rules:")
 print("Game has 5 rounds.")
 print("you will have 5 chances to make a choice.")
 print("You will win if you win 3 or more rounds.")
-count = 0
-for i in range(1,6):
-    print("\n\nround ",i)
+
+count_round=1
+win = 0
+lose = 0
+while count_round<6:
+    print("\n\nround ",count_round)
+
+    #computer and user's choice
     number=random.randint(1,3)  
     computer_map={1:"rock",2:"paper",3:"scissor"}
-    computer=computer_map[number]
-
     entry=input("enter your choice:(rock)r,(paper)p,(scissor)s: ").lower()  
     user_map={"s":"scissor","r":"rock","p":"paper"} 
 
-    if entry=="s" or entry=="p" or entry=="r":
-        print("you choose:", user_map[entry])
-        i+=1
-    else:
+    #cheak condition of valid entry
+    if entry not in user_map:
         print("invalid entry!!")
         print("please choose:(rock)r,(paper)p,(scissor)s ") 
-        print("we will not count this round")
-        exit()  
+        print("we will not count this round") 
+        continue
 
-    if number==1 or number==2 or number==3:
-        print("computer choose: ", computer)
-    else:
-        print("invalid choice made by the computer")
-        print("please try again!!")
+    #displays choices
+    print("you choose:", user_map[entry]) 
+    print("computer choose: ", computer_map[number])
+    
 
+    #evalute each round
     result=game(entry,number) 
-    if result==None:
-        print("invalid") 
-    else:     
-        print(result)         
+    if result == "won":
+        print("you won this round")
+        win += 1
+    elif result == "lose":
+        print("you lost this round")
+        lose += 1
+    else:
+        print("it's a tie this round!!")
 
+    count_round +=1     
+ 
+ # FINAL RESULT
+print("\n\nFinal Result:")
+print("Your wins:", win)
+print("Computer wins:", lose)   
+if win > lose:
+    print("You won the game")
+elif lose > win:
+    print("You lost the game")
+else:
+    print("The game is a draw")
